ba408ef23bae5ccb0623de1b36d909e3535df749
Slava Poliakov <78slavapolyakov@gmail.com>
2023-10-12 19:26:58 +0300

    Finished 2nd part

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryHTML = galleryItems.map(({ preview, original, description }) => {
+    return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>        
+    `;
+}).join('');
+
+const galleryList = document.querySelector('.gallery');
+galleryList.innerHTML = galleryHTML;
+
+let gallery = new SimpleLightbox(
+    '.gallery a',
+    {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250
+    }
+);
